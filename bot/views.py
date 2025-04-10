39246fd69c2040777832a8f261faeaf3dd16d82c
Matin <mohamad13840208@gmail.com>
2025-04-10 13:04:46 +0330

    new changes in bot.views

--- bot/views.py
+++ bot/views.py
@@ -1,9 +1,19 @@
+
+
+# @csrf_exempt
+# def telegram_webhook(request):
+#     if request.method == 'POST':
+#         data = json.loads(request.body.decode('utf-8'))
+#         print("📩 Webhook received:", data)
+#         return JsonResponse({'status': 'received'})
+#     return JsonResponse({'error': 'invalid request'}, status=400)
+
 import requests
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 
-TOKEN = 'توکن خودت رو بذار اینجا'
+TOKEN = '7687944134:AAExhPl0bOBKI2ID_qsi4fzEDVDhOW5urLw'
 
 @csrf_exempt
 def telegram_webhook(request):
