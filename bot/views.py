7cf07a5ecc4817a92ea0774eb4445b1a0cbbd74e
Matin <mohamad13840208@gmail.com>
2025-04-10 17:42:47 +0330

    modify views

--- bot/views.py
+++ bot/views.py
@@ -20,24 +20,42 @@ def telegram_webhook(request):
         ai_response = response.json()
 
 
-        if chat_id:
-            if text == "/start":
-                message = 'Hi! The bot lets you to access the best music according to your mood!Let\'s start, type your current mood.'
-            else:
-                if isinstance(ai_response, list):
-                    message = ai_response[0]["generated_text"]
-                else:
-                    message = "Sorry cant understand!"
+        # if chat_id:
+        #     if text == "/start":
+        #         message = 'Hi! The bot lets you to access the best music\
+        #               according to your mood!Let\'s start, type your current mood.'
             
-            requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage", data={
-                'chat_id' : chat_id,
-                'text' : message
-            })
+        #     requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage", data={
+        #         'chat_id' : chat_id,
+        #         'text' : message
+        #     })
+
+        i_response = query({
+            "inputs": text
+        })
+
+        if text=="/start":
+            response_text = "Hi! The bot lets you to access the best music\
+                according to your mood!Let\'s start, type your current mood."
+        else:
+            response_text = ai_response[0]["generated_text"] if isinstance(ai_response, list) else "Sorry! can\'t understand!🤔"
+
+        # response
+        send_telegram_message(chat_id, response_text)
 
         return JsonResponse({"status": "ok"})
 
     return JsonResponse({"error": "invalid request"}, status=400)
 
+
+def send_telegram_message(chat_id, text):
+    TELEGRAM_TOKEN = "7687944134:AAExhPl0bOBKI2ID_qsi4fzEDVDhOW5urLw"
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": chat_id,
+        "text": text
+    }
+    requests.post(url, json=payload)
 
 
 # Use Cerebras API
