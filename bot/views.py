7193e61d15a236e755333d43d017b9a6bdad98b1
Matin <mohamad13840208@gmail.com>
2025-04-11 11:50:36 +0330

    new changes:

--- bot/views.py
+++ bot/views.py
@@ -4,7 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 
 TOKEN = '7687944134:AAExhPl0bOBKI2ID_qsi4fzEDVDhOW5urLw'
-HF_URL = 'https://api.telegram.org/bot{TOKEN}/ai/song/'
+
 
 @csrf_exempt
 def telegram_webhook(request):
@@ -16,8 +16,7 @@ def telegram_webhook(request):
         text = data["message"]["text"]
 
         # send view to hugging face
-        response = requests.post(HF_URL, json={"message": text})
-        ai_response = response.json()
+
 
 
         ai_response = query({
@@ -28,9 +27,13 @@ def telegram_webhook(request):
             response_text = "Hi! The bot lets you to access the best music\
                 according to your mood!Let\'s start, type your current mood."
         else:
-            response_text = ai_response["generated_text"]\
-                 if isinstance(ai_response, list)\
-                     else "Sorry! can\'t understand!🤔"
+            try:
+                response_text = ai_response[0]["generated_text"]\
+                    if isinstance(ai_response, list)\
+                        else "Sorry! can\'t understand!🤔"
+            except Exception as e:
+                print("AI error:", e)
+                response_text = "Something went wrong with AI model! 😔"
 
         # response
         send_telegram_message(chat_id, response_text)
@@ -63,20 +66,6 @@ def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
 
-@csrf_exempt
-def song_recommendation(request):
-    if request.method == "POST":
-        body = json.loads(request.body)
-        user_input = body.get("message", "")
-     
-
-        result = query({
-            "inputs": user_input
-        })
-
-        return JsonResponse(result, safe=False)
-
-    return JsonResponse({"error": "Invalid request"}, status=400)
 
 
 def build_prompt(user_input):
@@ -84,7 +73,7 @@ def build_prompt(user_input):
 You are an AI that suggests songs based on the user's mood.
 User's mood: "I feel lonely and nostalgic"
 
-{user_input}
+"{user_input}"
 
 Only return in this format:
 Song: [song name]
