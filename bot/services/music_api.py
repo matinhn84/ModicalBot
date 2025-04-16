2a92b59e2ee5d8018dfd1a399a943fa736d612a8
Matin <mohamad13840208@gmail.com>
2025-04-16 12:30:25 +0330

    changes in get_song_info

--- bot/services/music_api.py
+++ bot/services/music_api.py
@@ -7,6 +7,8 @@ from rest_framework.response import Response
 def get_song_info(query):
 
     search_res = requests.get(f"https://saavn.dev/api/search?query={query}")
+    if not search_res.ok:
+        return Response({'error': 'No songs returned'}, status=404)
     songs = search_res.json().get('data', {}).get('songs', {}).get('results', [])
     if not songs:
         return Response({'error': 'No songs found'}, status=404)
@@ -19,8 +21,15 @@ def get_song_info(query):
 
     # Get MP3 link
     song_detail_res = requests.get(f"https://saavn.dev/api/songs?id={song_id}")
-    song_data = song_detail_res.json().get('data', [])[0]
-    mp3_link = next((x['link'] for x in song_data['downloadUrl'] if x['quality'] == '320kbps'), None)
+    if not song_detail_res.ok:
+        return Response({'error': 'No song-id found!'}, status=404)
+    song_data = song_detail_res.json().get('data', [])
+    if not song_data:
+        return Response({'error': 'No song_data found!'}, status=404)
+    download_urls = song_data[0].get('downloadUrl', [])
+    mp3_link = next((x['link'] for x in download_urls if x.get('quality') == '320kbps'), None)
+    if not mp3_link:
+        return Response({'error': 'No mp3 link for this song!'}, status=404)
 
     return Response({
         'title': title,
