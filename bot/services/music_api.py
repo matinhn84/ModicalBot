a5553721deadafd0a3ccc0777264ad7cb599aed2
Matin <mohamad13840208@gmail.com>
2025-04-16 16:58:09 +0330

    midify get_song_ingo

--- bot/services/music_api.py
+++ bot/services/music_api.py
@@ -21,9 +21,9 @@ def get_song_info(query):
 
         # Get MP3 link
         song_detail_res = requests.get(f"https://saavn.dev/api/search/songs?query={song_id}")
-        song_data = song_detail_res.json().get('data', [])
+        song_data = song_detail_res.json().get('data', []).get('results', [])
 
-        download_urls = song_data[0].get('downloadUrl', []).get('results', [])
+        download_urls = song_data[0].get('downloadUrl', [])
         mp3_link = next((x['link'] for x in download_urls if x.get('quality') == '320kbps'), None)
         if not mp3_link:
             return Response({'error': 'No mp3 link for this song!'}, status=404)
