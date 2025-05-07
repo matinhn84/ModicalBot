9cef9fb946a33ac07ce8544726f03f16baae9693
Matin <mohamad13840208@gmail.com>
2025-05-07 15:36:32 +0330

    modify views.py - debug

--- bot/views.py
+++ bot/views.py
@@ -122,7 +122,7 @@ def telegram_webhook(request):
         send_photo_with_button(chat_id,
                                 image_url,
                                 text,
-                                [[{'text': 'mp3', 'url': 'https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/89/ea/d2/89ead2b5-bd25-8c77-e058-3d25c7cc65f8/mzaf_12471994567789315969.plus.aac.ep.m4a'}, {'text': 'apple_music', 'url': 'https://music.apple.com/us/album/arms/511504015?i=511504017&mttnagencyid=s2n&mttnsiteid=125115&mttn3pid=Apple-Shazam&mttnsub1=Shazam_ios&mttnsub2=5348615A-616D-3235-3830-44754D6D5973&itscg=30201&app=music&itsct=Shazam_ios'}], [{'text': 'spotify', 'url': 'spotify:search:Arms%20Christina%20Perri'}, {'text': 'youtube_music', 'url': 'deezer-query://www.deezer.com/play?query=%7Btrack%3A%27Arms%27%20artist%3A%27Christina+Perri%27%7D'}], [{'text': 'soundcloud', 'url': None}]]
+                                [[{'text': 'mp3', 'url': 'https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/89/ea/d2/89ead2b5-bd25-8c77-e058-3d25c7cc65f8/mzaf_12471994567789315969.plus.aac.ep.m4a'}, {'text': 'apple_music', 'url': 'https://music.apple.com/us/album/arms/511504015?i=511504017&mttnagencyid=s2n&mttnsiteid=125115&mttn3pid=Apple-Shazam&mttnsub1=Shazam_ios&mttnsub2=5348615A-616D-3235-3830-44754D6D5973&itscg=30201&app=music&itsct=Shazam_ios'}],[{'text': 'spotify', 'url': 'spotify:search:Arms%20Christina%20Perri'}, {'text': 'youtube_music', 'url': 'deezer-query://www.deezer.com/play?query=%7Btrack%3A%27Arms%27%20artist%3A%27Christina+Perri%27%7D'}],[{'text': 'soundcloud', 'url': None}]]
                                )
 
 
